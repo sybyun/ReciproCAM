b301eedd1c2a2d1d3222c6d7390197562b70e9d9
Mark <sybyun@gmail.com>
2025-01-28 22:18:42 +0900

    Clearned files

--- util/util.py
+++ util/util.py
@@ -1,198 +1,27 @@
-'''Some helper functions for PyTorch, including:
-    - get_mean_and_std: calculate the mean and std value of dataset.
-    - msr_init: net parameter initialization.
-    - progress_bar: progress bar mimic xlua.progress.
-'''
-import os
-import sys
-import time
-import math
 import numpy as np
+from matplotlib import cm
+from PIL import Image
 
-import torch
-import torch.nn as nn
-import torch.nn.init as init
+# Function to create heatmap overlay
+def overlay_heatmap(img: Image.Image, mask: Image.Image, colormap: str = "jet", alpha: float = 0.7) -> Image.Image:
+    """Overlay a colormapped mask on a background image
 
+    Args:
+        img: background image
+        mask: mask to be overlayed in grayscale
+        colormap: colormap to be applied on the mask
+        alpha: transparency of the background image
 
-def get_mean_and_std(dataset):
-    '''Compute the mean and std value of dataset.'''
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2)
-    mean = torch.zeros(3)
-    std = torch.zeros(3)
-    print('==> Computing mean and std..')
-    for inputs, targets in dataloader:
-        for i in range(3):
-            mean[i] += inputs[:,i,:,:].mean()
-            std[i] += inputs[:,i,:,:].std()
-    mean.div_(len(dataset))
-    std.div_(len(dataset))
-    return mean, std
-
-def init_params(net):
-    '''Init layer parameters.'''
-    for m in net.modules():
-        if isinstance(m, nn.Conv2d):
-            init.kaiming_normal(m.weight, mode='fan_out')
-            if m.bias:
-                init.constant(m.bias, 0)
-        elif isinstance(m, nn.BatchNorm2d):
-            init.constant(m.weight, 1)
-            init.constant(m.bias, 0)
-        elif isinstance(m, nn.Linear):
-            init.normal(m.weight, std=1e-3)
-            if m.bias:
-                init.constant(m.bias, 0)
-
-
-_, term_width = os.popen('stty size', 'r').read().split()
-term_width = int(term_width)
-
-TOTAL_BAR_LENGTH = 35.
-last_time = time.time()
-begin_time = last_time
-def progress_bar(current, total, msg=None):
-    global last_time, begin_time
-    if current == 0:
-        begin_time = time.time()  # Reset for new bar.
-
-    cur_len = int(TOTAL_BAR_LENGTH*current/total)
-    rest_len = int(TOTAL_BAR_LENGTH - cur_len) - 1
-
-    sys.stdout.write(' [')
-    for i in range(cur_len):
-        sys.stdout.write('=')
-    sys.stdout.write('>')
-    for i in range(rest_len):
-        sys.stdout.write('.')
-    sys.stdout.write(']')
-
-    cur_time = time.time()
-    step_time = cur_time - last_time
-    last_time = cur_time
-    tot_time = cur_time - begin_time
-
-    L = []
-    #L.append('  Step: %s' % format_time(step_time))
-    #L.append(' | Tot: %s' % format_time(tot_time))
-    if msg:
-        #L.append(' | ' + msg)
-        L.append(msg)
-
-    msg = ''.join(L)
-    sys.stdout.write(msg)
-    for i in range(term_width-int(TOTAL_BAR_LENGTH)-len(msg)-3):
-        sys.stdout.write(' ')
-
-    # Go back to the center of the bar.
-    for i in range(term_width-int(TOTAL_BAR_LENGTH/2)+2):
-        sys.stdout.write('\b')
-    sys.stdout.write(' %d/%d ' % (current+1, total))
-
-    if current < total-1:
-        sys.stdout.write('\r')
-    else:
-        sys.stdout.write('\n')
-    sys.stdout.flush()
-
-def format_time(seconds):
-    days = int(seconds / 3600/24)
-    seconds = seconds - days*3600*24
-    hours = int(seconds / 3600)
-    seconds = seconds - hours*3600
-    minutes = int(seconds / 60)
-    seconds = seconds - minutes*60
-    secondsf = int(seconds)
-    seconds = seconds - secondsf
-    millis = int(seconds*1000)
-
-    f = ''
-    i = 1
-    if days > 0:
-        f += str(days) + 'D'
-        i += 1
-    if hours > 0 and i <= 2:
-        f += str(hours) + 'h'
-        i += 1
-    if minutes > 0 and i <= 2:
-        f += str(minutes) + 'm'
-        i += 1
-    if secondsf > 0 and i <= 2:
-        f += str(secondsf) + 's'
-        i += 1
-    if millis > 0 and i <= 2:
-        f += str(millis) + 'ms'
-        i += 1
-    if f == '':
-        f = '0ms'
-    return f
-
-
-#from pathlib import Path
-#from openvino.runtime import Core
-'''
-def torch_to_IR(x, torch_model, input_shape='[128, 3, 224, 224]', precision='FP32', device='cpu'):
-     # onnx convert
-    onnx_path = Path("torch_to_IR.onnx")
-    if not onnx_path.exists():
-        torch.onnx.export(torch_model, x, onnx_path, export_params=True,do_constant_folding=False,
-                        opset_version=11, input_names=['input'], output_names=['output'],)  
-        print(f"ONNX model exported to {onnx_path}.")      
-    else:
-        print(f"ONNX model {onnx_path} already exists.")       
+    Returns:
+        overlayed image
+    """
     
-    ir_path = Path("torch_to_IR.xml")
-    if not ir_path.exists():
-        mo_command = f"""mo
-                 --input_model "{onnx_path}"
-                 --input_shape "{input_shape}"
-                 --data_type "{precision}"
-                 --output_dir "./"
-                 """
-        mo_command = " ".join(mo_command.split())
-        os.system(mo_command)
-        print("Exporting ONNX model to IR... This may take a few minutes.")
-    else:
-        print(f"IR model {ir_path} already exists.")
+    cmap = cm.get_cmap(colormap)
+    # Resize mask and apply colormap
+    overlay = mask.resize(img.size, resample=Image.BICUBIC)
+    overlay = (255 * cmap(np.asarray(overlay) ** 2)[:, :, :3]).astype(np.uint8)
+    # Overlay the image with the mask
+    overlayed_img = Image.fromarray((alpha * np.asarray(img) + (1 - alpha) * overlay).astype(np.uint8))
+
+    return overlayed_img
     
-    ie = Core()
-    model_ir = ie.read_model(model=ir_path)
-    if device == 'igpu':
-        compiled_model_ir = ie.compile_model(model=model_ir, device_name="GPU")
-    else:
-        compiled_model_ir = ie.compile_model(model=model_ir, device_name="CPU")
-    request_ir = compiled_model_ir.create_infer_request()
-
-    return request_ir
-'''
-
-def NMS(boxes, overlapThresh = 0.4):
-    # Return an empty list, if no boxes given
-    if len(boxes) == 0:
-        return []
-    x1 = boxes[:, 0]  # x coordinate of the top-left corner
-    y1 = boxes[:, 1]  # y coordinate of the top-left corner
-    x2 = boxes[:, 2]  # x coordinate of the bottom-right corner
-    y2 = boxes[:, 3]  # y coordinate of the bottom-right corner
-    # Compute the area of the bounding boxes and sort the bounding
-    # Boxes by the bottom-right y-coordinate of the bounding box
-    areas = (x2 - x1 + 1) * (y2 - y1 + 1) # We add 1, because the pixel at the start as well as at the end counts
-    # The indices of all boxes at start. We will redundant indices one by one.
-    indices = np.arange(len(x1))
-    for i,box in enumerate(boxes):
-        # Create temporary indices  
-        temp_indices = indices[indices!=i]
-        # Find out the coordinates of the intersection box
-        xx1 = np.maximum(box[0], boxes[temp_indices,0])
-        yy1 = np.maximum(box[1], boxes[temp_indices,1])
-        xx2 = np.minimum(box[2], boxes[temp_indices,2])
-        yy2 = np.minimum(box[3], boxes[temp_indices,3])
-        # Find out the width and the height of the intersection box
-        w = np.maximum(0, xx2 - xx1 + 1)
-        h = np.maximum(0, yy2 - yy1 + 1)
-        # compute the ratio of overlap
-        overlap = (w * h) / areas[temp_indices]
-        # if the actual boungding box has an overlap bigger than treshold with any other box, remove it's index  
-        if np.any(overlap) > overlapThresh:
-            indices = indices[indices != i]
-    #return only the boxes at the remaining indices
-    return boxes[indices].astype(int)
